06e75eb74f0b02129670ee486019cb7f2ec91502
justkimtai <justkimtai@gmail.com>
2025-10-12 17:03:42 +0300

    passenger file fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,10 @@
-import sys, os
+import os
+import sys
+
 
 project_home = '/home/justuski/reliefapp'
 if project_home not in sys.path:
-    sys.path.append(project_home)
+    sys.path.insert(0, project_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relief_backend.settings')
 
