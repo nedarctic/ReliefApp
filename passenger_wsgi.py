acbdfb75f43d916be990b0fb81e47bc06cef5b2d
justkimtai <justkimtai@gmail.com>
2025-10-12 13:52:13 +0300

    configured path to be appended instead of being replaced

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,9 @@
 import sys, os
 
-project_home = u'/home/justuski/repositories/ReliefApp'
+project_home = '/home/justuski/repositories/ReliefApp'
 if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-    
+    sys.path.append(project_home)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relief_backend.settings')
 
 from django.core.wsgi import get_wsgi_application
